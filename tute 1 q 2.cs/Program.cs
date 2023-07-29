9538483a02d8820b1fe22dc630f53a40fc0b015a
navi5971 <140829902+navi5971@users.noreply.github.com>
2023-07-29 14:29:16 +0530

    lab 1 question 2

---
+++ tute 1 q 2.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute_1_q_2.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            double area;
+
+            Console.WriteLine("enter the radius :");
+            radius = int.Parse(Console.ReadLine());
+            Console.WriteLine("the area of a circle is " + (radius * radius * 3.14));
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
