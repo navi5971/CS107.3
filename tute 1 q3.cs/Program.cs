ffe5a35b9c69b23bbddeaeb91f803e10de2bb7e9
navi5971 <140829902+navi5971@users.noreply.github.com>
2023-07-31 02:45:13 +0530

    lab 1 question 3

---
+++ tute 1 q3.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute_1_q3.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+           
+
+            Console.WriteLine("enter the first number: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("the sum is :" +(num1 + num2));
+            Console.WriteLine("enter anything to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
